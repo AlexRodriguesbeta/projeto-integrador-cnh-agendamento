a3243d44ec465d241d4383ac6f87378b420063b9
Caio Paiva <caiopousa@gmail.com>
2020-09-27 22:34:01 -0300

    Script JS com os dados da API nova.
    O código necessário para fetch está comentado para que não seja feita uma requisição para API ao carregar o arquivo.
    Do jeito que está escrito, ele retorna somente a quarta linha da planilha (id=3); consulte a documentação do sheetson para mais detalhes.

---
+++ provisórios/js/teste.js
@@ -0,0 +1,17 @@
+// myHeaders = new Headers({
+//     "Authorization": "Bearer m_v4qYCfdSzeVczUDFbob_AkExEaVzYobSUhULVuGXYO1VOlJnlaSPt36oyT0Q",
+//     "X-Spreadsheet-Id": "1sHxAUBPyx-RnViA4dtb2M5Uhrr8TzBgL-a0fnJRoP_8",
+//   });
+
+// var myInit = { method: 'GET',
+//                headers: myHeaders,
+//                mode: 'cors',
+//                cache: 'default' };
+
+// fetch('https://api.sheetson.com/v2/sheets/BaseCNH/4',myInit)
+// .then(function(response) {
+//     let retorno = response.json();
+//     console.log(retorno);
+//     return retorno;
+// })
+
